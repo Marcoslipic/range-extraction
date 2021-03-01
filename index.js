f1e74a9bf3069c1938a8a55d542cb8f7661130e6
Marcoslipic <mjlipic.37@gmail.com>
2021-02-28 22:31:44 -0700

    output

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function solution(list){
     let index = 0;
+    let output = "";
 
     for (let i = 0; i < list.length; i++){
         if (list[i + 1] === )
     }
+    return output 
 }
 console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18,19, 20]))
